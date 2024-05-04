aa08b9fb8aacdc2fdfa9e9c47d87cf721b774058
Jules Robins <julesrobins.2021@scis.smu.edu.sg>
2024-05-04 16:48:31 +0800

    Fixed Nav Bar and Menu Overlay

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,27 +1,78 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Link from 'next/link'
+import NavLink from "./NavLink"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+import MenuOverlay from './MenuOverlay'
+
+const navLinks = [
+    {
+        title: "About",
+        path: "#about",
+    },
+    {
+        title: "Experience",
+        path: "#experience",
+    },
+    {
+        title: "Projects",
+        path: "#projects",
+    },
+    {
+        title: "Certifications",
+        path: "#certifications",
+    },
+    {
+        title: "Contact",
+        path: "#contact",
+    }
+]
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-            <Link href={"/"} className="text-5xl text-[#FFFFF3] font-semibold">
-                Logo
-            </Link>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-            <ul>
-                <li><Link
-                        href="#about"
-                        className="block py-2 pl-3 pr-4 text-[#FFFFF3] sm:text-xl rounded md:p-0"
-                    >
-                        About
-                    </Link>
-                </li>
-            </ul>
-        </div>
-        </div>
-    </nav>
-  )
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    return (
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#FF4365] bg-opacity-90">
+            <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+                <Link
+                    href={"/"}
+                    className="text-2xl md:text-5xl text-[#FFFFF3] font-semibold"
+                >
+                    Logo
+                </Link>
+                <div className="block md:hidden">
+                    {!navbarOpen ? (
+                        <button
+                            onClick={() => setNavbarOpen (true)}
+                            className="flex items-center px-3 py-2 text-slate-200 hover:text-white hover:border-white"
+                        >
+                            <Bars3Icon className="h-7 w-7"/>
+                        </button>
+                    ) : (
+                        <button
+                            onClick={() => setNavbarOpen (false)}
+                            className="flex items-center px-3 py-2 text-slate-200 hover:text-white hover:border-white"
+                        >
+                            <XMarkIcon className="h-7 w-7"/>
+                        </button>
+                    )}
+                </div>
+                <div className="menu hidden md:block md:w-auto" id="navbar">
+                    <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+                        {navLinks.map((link, index) => (
+                            <li key={index}>
+                                <NavLink
+                                    href={link.path}
+                                    title={link.title}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+        </nav>
+    )
 }
 
 export default Navbar
